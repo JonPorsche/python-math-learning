5d4ddab43546cf30db9c2803ebd143ffbe57d84b
JonPorsche <jonespor@gmail.com>
2022-08-26 12:48:09 +0200

    Quadratische Matrix mit Listen

---
+++ quadratische_matrix.py
@@ -0,0 +1,18 @@
+# 05_liste2.py
+
+a = [[3, 2, 1],
+     [4, 5, 6],
+     [9, 8, 7]]
+
+print("3 x 3 Matrix")
+print(a[0])
+print(a[1])
+print(a[2])
+
+print("Elemente der Matrix")
+print("a11 = ", a[0][0])
+print("a22 = ", a[1][1])
+print("a33 = ", a[2][2])
+print("a12 = ", a[0][1])
+print("a21 = ", a[1][0])
+
