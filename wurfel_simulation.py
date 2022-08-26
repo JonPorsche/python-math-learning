021cbadc838fdc7168ad34243d661b8e30ec3f9b
JonPorsche <jonespor@gmail.com>
2022-08-26 10:29:24 +0200

    Würfelsimulation

---
+++ wurfel_simulation.py
@@ -0,0 +1,9 @@
+# 01_tupel1.py
+
+from random import randint
+
+(a, b, c) = randint(1, 6), randint(1, 6), randint(1, 6)
+print("Würfelsimulation")
+print("Würfel a", a)
+print("Würfel a", b)
+print("Würfel a", c)
