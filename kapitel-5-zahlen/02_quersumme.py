87f0c3420aed07acf849dfe853a3df1c1e593ae3
JonPorsche <jonespor@gmail.com>
2022-09-21 10:12:23 +0200

    Quersumme

---
+++ kapitel-5-zahlen/02_quersumme.py
@@ -0,0 +1,27 @@
+# 02_quersumme.py
+
+# Division mit Rest (klassische Lösung)
+def quersumme1(z):
+    qs = 0
+    while z != 0:
+        qs = qs + z % 10
+        z = z // 10  # Ganzzahldivision
+    return qs
+
+
+# typische Lösung mit Python
+def quersumme2(z):
+    str_zahl = str(z)
+    qs = 0
+    for ziffer in str_zahl:
+        qs = qs + int(ziffer)
+    return qs
+
+
+# Hauptprogramm
+zahl = 123456789
+
+# q = quersumme1(zahl)
+q = quersumme2(zahl)
+
+print("\nDie Quersume von", zahl, "ist", q)
