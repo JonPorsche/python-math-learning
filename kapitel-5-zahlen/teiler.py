bed58cb7eb5e44e0add3e26d01c96f68bdcd9159
JonPorsche <jonespor@gmail.com>
2022-09-22 21:36:45 +0200

    ggT -> größte gemeinsamer Teiler

---
+++ kapitel-5-zahlen/teiler.py
@@ -0,0 +1,16 @@
+# 04_teiler.py
+def berechne_teiler(zahl):
+    teiler = []
+    for t in range(2, zahl // 2 + 1):
+        if zahl % t == 0:
+            teiler.append(t)
+        else:
+            continue
+    return teiler
+
+
+# Hauptprogram
+z1 = int(input())
+z2 = int(input())
+print("Teiler von", z1, "=", berechne_teiler(z1))
+print("Teiler von", z2, "=", berechne_teiler(z2))
